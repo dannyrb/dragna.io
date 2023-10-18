20909b7283722d969c0e60ac7c8258ab018b9679
Danny Brown <dannyb@hey.com>
2023-10-18 11:03:28 -0400

    lint:fix

--- src/components/GridItemApp/GridItemApp.tsx
+++ src/components/GridItemApp/GridItemApp.tsx
@@ -5,7 +5,16 @@ function GridItemApp () {
     return (
         <a className="app" href="/iterm">
             <div className="icon">
-                <img alt="iTerm" loading="lazy" width="200" height="200" decoding="async" data-nimg="1" src="/icons/used/pack-7/042-haunted house.svg" style="color: transparent;" />
+                <img 
+                    alt="iTerm" 
+                    loading="lazy" 
+                    width="200" 
+                    height="200" 
+                    decoding="async" 
+                    data-nimg="1" 
+                    src="/icons/used/pack-7/042-haunted house.svg" 
+                    style={{color: 'transparent'}} 
+                />
             </div>
                 <div className="content">
                     <span className="title">iTerm</span>
